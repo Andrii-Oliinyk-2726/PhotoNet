3fea62c59e0d9411d4a19d44a5fb8e2636acc094
Artur Mistiuk <mistiuk.artur@gmail.com>
2023-07-26 14:23:45 +0200

    Update db.py

--- src/database/db.py
+++ src/database/db.py
@@ -4,8 +4,9 @@ from sqlalchemy.exc import DatabaseError
 
 from src.conf.config import settings
 
-
-SQLALCHEMY_DATABASE_URL = settings.sqlalchemy_database_url
+# SQLite for testing
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+# SQLALCHEMY_DATABASE_URL = settings.sqlalchemy_database_url
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True, max_overflow=5)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
