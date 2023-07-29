358e310e58ac9611a8ca4afe9dcf9d6dad8257ac
Artur Mistiuk <mistiuk.artur@gmail.com>
2023-07-29 19:56:56 +0200

    Update users.py

--- src/routes/users.py
+++ src/routes/users.py
@@ -11,31 +11,31 @@ from src.repository import users as repository_users
 router = APIRouter(prefix="/users", tags=["users"])
 
 
-@router.get("/users", response_model=List[UserResponse], tags=["users"])
+@router.get("/", response_model=List[UserResponse], tags=["users"])
 async def get_users(db: Session = Depends(get_db)):
     users = await repository_users.get_users(db)
     return users
 
 
-@router.get("/users/{user_id}", response_model=UserResponse, tags=["users"])
+@router.get("/{user_id}", response_model=UserResponse, tags=["users"])
 async def get_user(user_id: int = Path(ge=1), db: Session = Depends(get_db)):
     user = await repository_users.get_user(user_id, db)
     return user
 
 
-@router.post("/users", response_model=UserResponse, tags=["users"])
+@router.post("/", response_model=UserResponse, tags=["users"])
 async def create_user(body: UserModel, db: Session = Depends(get_db)):
     user = await repository_users.create_user(body, db)
     return user
 
 
-@router.put("/users/{user_id}", response_model=UserResponse, tags=["users"])
+@router.put("/{user_id}", response_model=UserResponse, tags=["users"])
 async def update_user(body: UserModel, user_id: int = Path(ge=1), db: Session = Depends(get_db)):
     user = await repository_users.update_user(body, user_id, db)
     return user
 
 
-@router.delete("/users/{user_id}", response_model=UserResponse, tags=["users"])
+@router.delete("/{user_id}", response_model=UserResponse, tags=["users"])
 async def delete_user(user_id: int = Path(ge=1), db: Session = Depends(get_db)):
 
     user = await repository_users.delete_user(user_id, db)
